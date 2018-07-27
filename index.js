cc2c6cce7d1701b92addc83c49ce95b3853c5211
Bruno Pimentel <bpimentel@mastertech.tech>
2018-07-27 09:28:33 -0300

    Refatorar o código em funções

--- index.js
+++ index.js
@@ -5,32 +5,53 @@ const email = process.argv[4];
 
 let cadastros = {};
 
-fs.readFile('cadastros.json',  (err, dados) => {
+const comandos = {
+    'salvar': salvar,
+    'buscar': buscar,
+    'buscar-todos': buscarTodos       
+}
 
+fs.readFile('cadastros.json',  (err, dados) => {
     if(!err){
         let dadosString = dados.toString();
         cadastros = JSON.parse(dadosString);
     }
-    
-    if(comando === 'salvar'){
-        cadastros[nome] = email;
-    
-        const dadosGravacao = JSON.stringify(cadastros);
-    
-        fs.writeFile('cadastros.json', dadosGravacao, (err) => {
-            if(err){
-                console.log('Deu ruim');
-            }else{
-                console.log('Gravei o arquivo');
-            }
-        });
-    
-    }else if(comando === 'buscar-todos'){
-        console.log(cadastros);
-    }else if(comando == 'buscar'){
-        console.log(cadastros[nome]);
-    }else{
-        console.log('Tá bebo?');
+
+    if(!comandos[comando]){
+        console.log('Tá bebo! Esse comando não existe, pows!');
+        return
     }
-    
+
+    comandos[comando]();
 });
+
+function salvar(){
+    if(!nome || !email){
+        console.log('Nome e email são obrigatórios, cacilda!');
+        return;
+    }
+
+    cadastros[nome] = email;
+    
+    const dadosGravacao = JSON.stringify(cadastros);
+    
+    fs.writeFile('cadastros.json', dadosGravacao, (err) => {
+        if(err){
+            console.log('Deu ruim');
+        }else{
+            console.log('Gravei o arquivo');
+        }
+    });    
+}
+
+function buscarTodos(){
+    console.log(cadastros);
+}
+
+function buscar(){
+    if(!cadastros[nome]){
+        console.log('Num achei!');
+    }else{
+        console.log(cadastros[nome]);
+    }
+}
